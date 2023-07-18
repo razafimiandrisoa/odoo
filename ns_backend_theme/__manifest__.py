2a76578bfeb21dd344158c963aa102b76beeb653
Noarison <lrazafimiandrisoa@gmail.com>
2023-07-18 15:06:14 +0300

    [ADD] - Ajout module theme backend for odoo 16

---
+++ ns_backend_theme/__manifest__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Noasource backend theme",
+    'sequence': -4,
+    'summary': """
+        Inherit web_responsive module
+    """,
+    'description': """
+        Backend theme for odoo v16 community
+    """,
+    'author': "Noarison Léonce RAZAFIMIANDRISOA",
+    "maintainers": ["Noarison Léonce RAZAFIMIANDRISOA (lrazafimaindrisoa@gmail.com)"],
+    'category': 'Themes/Backend',
+    'version': '16.1.0.0',
+    'depends': ['base', 'web_responsive'],
+    "excludes": ["web_enterprise"],
+    'data': [
+    ],
+    'images': [
+        'static/description/icon.png',
+        'static/description/1.png',
+        'static/description/2.png',
+    ],
+    'assets': {
+        "web.assets_backend": [
+            "/ns_backend_theme/static/src/js/sidebar_menu.js",
+            "/ns_backend_theme/static/src/scss/loading.scss",
+            "/ns_backend_theme/static/src/scss/sidebar.scss",
+            "/ns_backend_theme/static/src/scss/navbar.scss",
+            "/ns_backend_theme/static/src/xml/top_bar.xml",
+        ],
+        # "web.assets_qweb": [
+        #     "/ns_backend_theme/static/src/xml/top_bar.xml",
+        # ],
+    },
+    "license": "LGPL-3",
+
+}
